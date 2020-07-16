711adf687d9d855fa478f40781fbdc52390f88b3
Szymon Jakóbczyk <sjakobczyk@gmail.com>
2020-07-16 04:10:49 +0200

    get the euler angles

--- Reader.py
+++ Reader.py
@@ -2,10 +2,13 @@
 import cv2
 import dlib
 import numpy as np
+import os
 #Set up some required objects
+#video_capture = cv2.VideoCapture(0) #Webcam object
 video_capture = cv2.VideoCapture('Source.mp4') #Webcam object
 detector = dlib.get_frontal_face_detector() #Face detector
 predictor = dlib.shape_predictor("Dataset/shape_predictor_68_face_landmarks.dat") #Landmark identifier. Set the filename to whatever you named the downloaded file
+
 while(video_capture.isOpened()):
     ret, frame = video_capture.read()
     if ret == False:
@@ -19,11 +22,11 @@ while(video_capture.isOpened()):
     # 3D model points.
     model_points = np.array([
         (0.0, 0.0, 0.0),  # Nose tip
-        (0.0, -330.0, -65.0),  # Chin
-        (-225.0, 170.0, -135.0),  # Left eye left corner
-        (225.0, 170.0, -135.0),  # Right eye right corner
-        (-150.0, -150.0, -125.0),  # Left Mouth corner
-        (150.0, -150.0, -125.0)  # Right mouth corner
+        (0.0, -210.0, -100.0),  # Chin
+        (-130.0, 90.0, -110.0),  # Left eye left corner
+        (130.0, 90.0, -110.0),  # Right eye right corner
+        (-180.0, -100.0, -280.0),  # Left Jaw
+        (180.0, -100.0, -280.0)  # Right Jaw
 
     ])
 
@@ -86,21 +89,21 @@ while(video_capture.isOpened()):
             (shape.part(8).x, shape.part(8).y),  # Chin
             (shape.part(45).x, shape.part(45).y),  # Left eye left corner
             (shape.part(36).x, shape.part(36).y),  # Right eye right corner
-            (shape.part(12).x, shape.part(12).y),  # Left Mouth corner
-            (shape.part(4).x, shape.part(4).y)  # Right mouth corner
+            (shape.part(12).x, shape.part(12).y),  # Left Jaw
+            (shape.part(4).x, shape.part(4).y)  # Right Jaw
         ], dtype="double")
 
-        print(
-        "Camera Matrix :\n {0}".format(camera_matrix))
+        #print(
+        #"Camera Matrix :\n {0}".format(camera_matrix))
 
         dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
         (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix,
                                                                       dist_coeffs)
 
-        print(
-        "Rotation Vector:\n {0}".format(rotation_vector))
-        print(
-        "Translation Vector:\n {0}".format(translation_vector))
+        #print(
+        #"Rotation Vector:\n {0}".format(rotation_vector))
+        #print(
+        #"Translation Vector:\n {0}".format(translation_vector))
 
         # Project a 3D point (0, 0, 1000.0) onto the image plane.
         # We use this to draw a line sticking out of the nose
@@ -109,12 +112,34 @@ while(video_capture.isOpened()):
                                                          translation_vector, camera_matrix, dist_coeffs)
 
         for p in image_points:
-            cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+            cv2.circle(frame, (int(p[0]), int(p[1])), 2, (255, 0, 0), -1)
 
         p1 = (int(image_points[0][0]), int(image_points[0][1]))
         p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
 
-        cv2.line(frame, p1, p2, (255, 0, 0), 2)
+        cv2.arrowedLine(frame, p1, p2, (255, 0, 0), 1)
+
+        rotation_matrix = np.zeros((3, 3))
+        cv2.Rodrigues(rotation_vector,rotation_matrix,jacobian)
+
+        projection_matrix = np.array(
+            [[rotation_matrix[0][0], rotation_matrix[0][1], rotation_matrix[0][2] , translation_vector[0]],
+             [rotation_matrix[1][0], rotation_matrix[1][1], rotation_matrix[1][2], translation_vector[1]],
+             [rotation_matrix[2][0], rotation_matrix[2][1], rotation_matrix[2][2] , translation_vector[2]]], dtype="double"
+        )
+
+        euler_angles = cv2.decomposeProjectionMatrix(projection_matrix)
+
+        euler_angles = euler_angles[6]
+        cv2.putText(frame,
+                    'yaw = '+ str(euler_angles[1][0]), (32, 32), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.75, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame,
+                    'pitch = ' + str(euler_angles[0][0]), (32, 64), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.75, (255, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame,
+                    'roll = ' + str(euler_angles[2][0]), (32, 96), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.75, (255, 0, 0), 1, cv2.LINE_AA)
 
     cv2.imshow("image", frame) #Display the frame
     if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
